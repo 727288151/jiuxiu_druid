28e1b70baf1beb7f6b41cdec05f9b957167d1e02
727288151 <727288151@qq.com>
2018-03-07 19:08:16 +0800

    init

---
+++ src/main/java/com/hualing365/jiuxiu/MainApplication.java
@@ -0,0 +1,21 @@
+package com.hualing365.jiuxiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		//return super.configure(builder);
+		return builder.sources(MainApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hualing365/jiuxiu/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.hualing365.jiuxiu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello spring boot";
+	}
+
+	@RequestMapping("/hi")
+	//@ResponseBody
+	public String hi(){
+		return "hi";
+	}
+}
