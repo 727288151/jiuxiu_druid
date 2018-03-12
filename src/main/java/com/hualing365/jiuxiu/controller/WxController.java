19b9bc8c93ca8bea5c111884645b40c8d0770e60
727288151 <727288151@qq.com>
2018-03-12 21:46:10 +0800

    Wechat fixed wxVerify request receive parameter method

--- src/main/java/com/hualing365/jiuxiu/controller/WxController.java
+++ src/main/java/com/hualing365/jiuxiu/controller/WxController.java
@@ -3,7 +3,6 @@
  */
 package com.hualing365.jiuxiu.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +17,7 @@ import com.hualing365.jiuxiu.wx.SignUtil;
 public class WxController {
 
 	@RequestMapping("/wxVerify")
-	public String wxVerify(@PathVariable String signature, @PathVariable String timestamp, @PathVariable String nonce, @PathVariable String echostr){
+	public String wxVerify(String signature, String timestamp, String nonce, String echostr){
 		// 1、微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。   
         // 2、时间戳   
         // 3、随机数   
